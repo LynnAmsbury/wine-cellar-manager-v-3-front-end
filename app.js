2ae64dfd31cf7449af2e98c50ed35ec8eba4a0f4
LynnAmsbury <Lynn8542@gmail.com>
2020-09-27 15:45:01 -0600

    Added create-wine form with attributes of variety, producer, region, vintage, and notes to index.html. Added wines-container to index.html. In app.js, added logic to create a new wine from information in the HTML create-wine form, added displayWine functionality to render a newly added wine to the display page, and added showWines logic and functionality to render all wines to the display page.

---
+++ app.js
@@ -0,0 +1,46 @@
+const baseURL = 'http://localhost:3000/'
+const winesURL = `${baseURL}/wines`
+
+fetch(winesURL)
+    .then(response => response.json())
+    .then(showWines)
+
+const createWine = document.querySelector('#create-wine')
+const winesContainer = document.querySelector('#wines-container')
+
+createWine.addEventListener('submit', (event)=>{
+    event.preventDefault();
+    
+    const createFormData = new FormData(event.target);
+    const variety = createFormData.get('variety');
+    const producer = createFormData.get('producer');
+    const region = createFormData.get('region');
+    const vintage = createFormData.get('vintage');
+    const notes = createFormData.get('notes');
+
+    const wine = {
+        variety: variety,
+        producer: producer,
+        region: region,
+        vintage: vintage,
+        notes: notes
+    }
+
+    displayWine(wine);
+})
+
+function displayWine(wine){
+    winesContainer.innerHTML += `
+    <div>
+      <h2>${wine.variety}</h2>
+      <h3>${wine.producer}</h3>
+      <h3>${wine.region}</h3>
+      <h3>${wine.vintage}</h3>
+      <h3>${wine.notes}</h3>
+    </div>
+  `
+}
+
+function showWines(wines){
+    wines.forEach(displayWine);
+}
