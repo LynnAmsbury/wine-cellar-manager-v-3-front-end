ed2db283f3e36bfe94c2289ac90ae895f8f1a909
LynnAmsbury <Lynn8542@gmail.com>
2020-10-01 18:29:36 -0600

    Added logic and functionality to edit a wine entry to the displayWine function; tested on the front and back ends.

--- app.js
+++ app.js
@@ -2,87 +2,144 @@ const baseURL = "http://localhost:3000/";
 const winesURL = `${baseURL}/wines`;
 
 fetch(winesURL)
-  .then((response) => response.json())
-  .then(showWines);
+    .then((response) => response.json())
+    .then(showWines);
 
-const createWine = document.querySelector("#create-wine");
+const createWineForm = document.querySelector("#create-wine-form");
 const winesContainer = document.querySelector("#wines-container");
 
-createWine.addEventListener("submit", addWine);
+createWineForm.addEventListener("submit", addWine);
 
 function addWine(event) {
-  event.preventDefault();
+    event.preventDefault();
 
-  const createWineFormData = new FormData(event.target);
-  const variety = createWineFormData.get("variety");
-  const producer = createWineFormData.get("producer");
-  const region = createWineFormData.get("region");
-  const vintage = createWineFormData.get("vintage");
-  const notes = createWineFormData.get("notes");
+    const createWineFormData = new FormData(event.target);
+    const variety = createWineFormData.get("variety");
+    const producer = createWineFormData.get("producer");
+    const region = createWineFormData.get("region");
+    const vintage = createWineFormData.get("vintage");
+    const notes = createWineFormData.get("notes");
 
-  const wine = {
+    const wine = {
     variety: variety,
     producer: producer,
     region: region,
     vintage: vintage,
     notes: notes,
-  };
+    };
 
-  persistWine(wine);
+    persistWine(wine);
+    event.target.reset();
 }
 
 function showWines(wines) {
-  wines.forEach(displayWine);
+    wines.forEach(displayWine);
 }
 
-// function displayWine(wine){
-//     winesContainer.innerHTML += `<ul id="wine-list-item">
-//         <li>${wine.variety}, ${wine.producer}, ${wine.region}, ${wine.vintage} ${wine.notes}</li>
-//         <button onclick=${deleteWine}>Remove This Wine</button>
-//     </ul>`
-// }
-
 function displayWine(wine) {
-  const wineCard = document.createElement("div");
-  wineCard.classList.add("wine-card");
+    const wineCard = document.createElement("div");
+    wineCard.classList.add("wine-card");
 
-  const wineVariety = document.createElement("h2");
-  wineVariety.textContent = wine.variety;
-  const wineProducer = document.createElement("p");
-  wineProducer.textContent = wine.producer;
-  const wineRegion = document.createElement("p");
-  wineRegion.textContent = wine.region;
-  const wineVintage = document.createElement("p");
-  wineVintage.textContent = wine.vintage;
-  const wineNotes = document.createElement("p");
-  wineNotes.textContent = wine.notes;
+    const wineVariety = document.createElement("h2");
+    wineVariety.textContent = wine.variety;
+    const wineProducer = document.createElement("p");
+    wineProducer.textContent = wine.producer;
+    const wineRegion = document.createElement("p");
+    wineRegion.textContent = wine.region;
+    const wineVintage = document.createElement("p");
+    wineVintage.textContent = wine.vintage;
+    const wineNotes = document.createElement("p");
+    wineNotes.textContent = wine.notes;
 
-  const deleteButton = document.createElement("button");
-  deleteButton.textContent = "Remove Wine";
-  deleteButton.addEventListener("click", ()=>deleteWine(wine.id, wineCard));
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Remove Wine";
+    deleteButton.addEventListener("click", ()=>deleteWine(wine.id, wineCard));
 
-  wineCard.append(
+    const editForm = document.createElement('form');
+
+    let editVariety = document.createElement('input');
+    editVariety.type = 'text';
+    editVariety.name = 'variety';
+    editVariety.placeholder = 'Variety Name';
+    editVariety.value = wine.variety;
+
+    let editProducer = document.createElement('input');
+    editProducer.type = 'text';
+    editProducer.name = 'producer';
+    editProducer.placeholder = 'Producer';
+    editProducer.value = wine.producer;
+
+    let editRegion = document.createElement('input');
+    editRegion.type = 'text';
+    editRegion.name = 'region';
+    editRegion.placeholder = 'Region';
+    editRegion.value = wine.region;
+
+    let editVintage = document.createElement('input');
+    editVintage.type = 'number';
+    editVintage.name = 'vintage';
+    editVintage.placeholder = 'Vintage Year';
+    editVintage.value = wine.vintage;
+
+    let editNotes = document.createElement('input');
+    editNotes.type = 'text';
+    editNotes.name = 'notes';
+    editNotes.placeholder = 'Notes';
+    editNotes.value = wine.notes;
+
+    const editFormSubmitButton = document.createElement('input');
+    editFormSubmitButton.type = 'submit';
+    editFormSubmitButton.value = 'Update Entry';
+
+    editForm.addEventListener('submit', (event)=>{
+        event.preventDefault();
+        const editFormData = new FormData(event.target);
+        const variety = editFormData.get('variety');
+        const producer =  editFormData.get('producer');
+        const region =  editFormData.get('region');
+        const vintage = editFormData.get('vintage');
+        const notes = editFormData.get('notes');
+
+        const updatedWine = { variety, producer, region, vintage, notes };
+
+        wineVariety.textContent = variety;
+        wineProducer.textContent = producer;
+        wineRegion.textContent = region;
+        wineVintage.textContent = vintage;
+        wineNotes.textContent = notes;
+        
+        fetch(`${winesURL}/${wine.id}`, {
+            method: 'PATCH',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(updatedWine)
+        })
+        
+    });
+
+    editForm.append(editVariety, editProducer, editRegion, editVintage, editNotes, editFormSubmitButton);
+    wineCard.append(
     wineVariety,
     wineProducer,
     wineRegion,
     wineVintage,
     wineNotes,
-    deleteButton
-  );
-  winesContainer.append(wineCard);
+    deleteButton,
+    editForm
+    );
+    winesContainer.append(wineCard);
 }
 
 function deleteWine(wineId, wineCard) {
-  wineCard.remove();
-  fetch(`${baseURL}/wines/${wineId}`, { method: "DELETE" });
+    wineCard.remove();
+    fetch(`${winesURL}/${wineId}`, { method: "DELETE" });
 }
 
 function persistWine(wine) {
-  fetch(winesURL, {
+    fetch(winesURL, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(wine),
-  })
+    })
     .then((response) => response.json())
     .then((wine) => displayWine(wine));
 }
